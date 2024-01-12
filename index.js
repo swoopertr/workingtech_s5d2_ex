e99c95e55dc92daac2834f72d15786151a8fffb4
swoopertr <tunc.kiral@gmail.com>
2024-01-12 10:53:54 +0300

    last commit

--- index.js
+++ index.js
@@ -1,81 +1,87 @@
-// 1 .Bir HTML sayfasında bulunan bir başlık elementini (h1) JavaScript kullanarak değiştirin. Yeni başlık metni "Merhaba, Dünya!" olsun.
-// 2. Bir HTML sayfasında bulunan bir div elementinin arka plan rengini JavaScript kullanarak değiştirin.
-// 3. Bir HTML sayfasında bulunan bir ul (sırasız liste) elementine JavaScript kullanarak yeni liste elemanları ekleyin.
-// 4. Bir düğmeye tıklandığında bir metni güncelleyen bir JavaScript fonksiyonunu çağırın.
-// 5. Bir HTML formunda kullanıcıdan bir metin girmesini isteyin. Ardından, girilen metni başka bir bölümde görüntüleyin.
-// 6. Bir HTML sayfasında bulunan bir div elementinin (veya istediğiniz bir elementin) stilini JavaScript kullanarak değiştirin. Örneğin, genişliğini artırın ve rengini değiştirin.
-// 7. Bir HTML sayfasında bulunan bir sırasız listeyi (ul) JavaScript kullanarak filtreleyin. Örneğin, sadece belirli bir harfi içerenleri gösterin.
-// 8. Bir HTML sayfasında bulunan bir sırasız listeye JavaScript kullanarak yeni bir liste elemanı ekleyin ve ardından bir elemanı silin.
-// 9. Bir HTML sayfasında bulunan bir liste üzerinde olay delegasyonu kullanarak tıklanan elemanın metnini gösterin.
-// 10. Bir HTML formunu JavaScript kullanarak doğrulayın. Örneğin, kullanıcı adı ve şifre alanlarını kontrol edin ve hatalı giriş durumunda bir uyarı gösterin.
+//global vars.
+
+//global objs.
+let dom_Helper = {
+  putDomInBody: function (dom) {
+    let body = document.getElementsByTagName("body")[0];
+    body.appendChild(dom);
+  },
+
+  addChildToParent: function(parent, child){
+    parent.appendChild(child);
+  },
+
+  createDomObj: function (tagName, className) {
+    let temp_dom = document.createElement(tagName);
+    if (className !== undefined) {
+      temp_dom.classList.add(className);
+    }
+    return temp_dom;
+  },
+};
+
+let div_container = {
+  div_imgs :[],
+  init: function () {
+    let tmp_div = this.createContainerDiv();
+    dom_Helper.putDomInBody(tmp_div);
+  },
+  createContainerDiv: function () {
+    //burda şunu yaptım bunu yaptım...
+    let tmp_div = dom_Helper.createDomObj("div", "container");
+    return tmp_div;
+  },
+  generateImgs: function(){
+    let _self = this;
+    data_obj.getDataFromApi("http://googleapis.com/getImgs",function(imgs){
+      _self.div_imgs = imgs;
+      //div_container.div_imgs = imgs;
+      let containerDiv = document.getElementsByClassName('container')[0];
+      
+      for (let i = 0; i < imgs.length; i++) {
+        const imgUrl = imgs[i];
+        //console.log("imgUrl", imgUrl);
+        let tmp_img_dom = img_obj.createImgDom();
+        img_obj.loadImage(tmp_img_dom, imgUrl);
+        dom_Helper.addChildToParent(containerDiv , tmp_img_dom);
+      }
+      
+    });
+  }
+};
+
+let img_obj = {
+  createImgDom: function(){
+    let tmp_img = dom_Helper.createDomObj('img');
+    return tmp_img;
+  },
+  loadImage : function(img_dom, imgUrl){
+    img_dom.setAttribute('src', imgUrl);
+  },
+
+}
+
+
+let data_obj = {
+  getDataFromApi: function(apiUrl, cb){
+   let data = [
+     "https://fastly.picsum.photos/id/833/300/300.jpg?hmac=m33-N82Dblxw-Bzcfdv95OhsW2L00s_mLYjpSM6aR2k",
+     "https://fastly.picsum.photos/id/288/300/300.jpg?hmac=7RMC2BTzA6EpogvGf74Us4VguwkoeSsLzBARJbs5VOk",
+     "https://fastly.picsum.photos/id/390/300/300.jpg?hmac=vIwFiXdW16lazu7WfEtZYsQ3UJSXWKISG7tOiTT-nhc",
+     "https://fastly.picsum.photos/id/790/300/300.jpg?hmac=jXjdU1D_tMJC9_oX744nIo2DeE65T9ri0pJUM3k86E8",
+     "https://fastly.picsum.photos/id/90/300/300.jpg?hmac=QT72Dh2MxQaVSEq6qSwRCE38wKIshGRJMffXMzgK0_Q",
+     "https://fastly.picsum.photos/id/236/300/300.jpg?hmac=foqe5XWYGwVxei7jB9_mh6DQDjOydIus_sTfpOuB2M8",
+   ];
+   //img_arr = data;
+    setTimeout(function () {
+      
+      cb && cb(data);
+    },2000);
+ }
+}
 
 document.addEventListener("DOMContentLoaded", function () {
-  updateFirstTitle();
-  backGroundMyDiv();
-  appendLi();
-  divStyleChange();
+  div_container.init();
+  div_container.generateImgs();
 });
-let a = 1;
-let b = 2;
-let c = a + b;
-let d = a - b;
 
-//todo send rocket to space....
-function updateFirstTitle() {
-  const newh1 = document.getElementById("myh1");
-  newh1.innerText = "Merhaba, Dünya!";
-}
-function myfn() {
-  console.log("done");
-}
-let abc = function (x,y) {
-  return x + y;
-}
-
-function AddText() {
-  let newText = "deneme metin içerik";
-  let myh1 = document.getElementById("myh1");
-  myh1.innerText = newText;
-}
-
-function backGroundMyDiv() {
-  const mydiv = document.getElementById("mydiv");
-  mydiv.style.backgroundColor = "red";
-}
-
-function appendLi() {
-  let ul = document.getElementById("101");
-  let createdLi = document.createElement("li");
-
-  createdLi.innerText = "Liste 3";
-
-  ul.appendChild(createdLi);
-}
-
-function metniGoruntule () {
-    let girilenMetin = document.getElementsByName("fname")[0];
-    let girilenMetinDegeri = girilenMetin.value
-    let myh1 = document.getElementById("myh1");
-    myh1.innerText = girilenMetinDegeri;
-}
-function fn1(){
-  console.log("fn1");
-}
-function submitBtn (event) {
-    event.preventDefault();
-    metniGoruntule();
-}
-
-function divStyleChange () {
-    //background-Color = "brown" ok
-    //font-family="arial" ok
-    //font-size ="22px"ok
-    //width: "300px"ok
-    //height; "610px"ok
-    let mydiv = document.getElementById("mydiv");
-    mydiv.style.backgroundColor = "hsl(0, 0%, 15%)";
-    mydiv.style.fontFamily = "Arial"
-    mydiv.style.fontSize ="22px"
-    mydiv.style.width = "300px"
-    mydiv.style.height = "610px"
-}
